783b220b0a4f22e35cd9bdbaa728f59a7d01cead
jpetersen5 <jasonpetersen707@gmail.com>
2024-06-26 16:09:49 -0700

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dmbot-web/',
+  base: "/dmbot-web/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].[hash].js`,
