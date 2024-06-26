a088a22e8708c1eda396f083ce1a2b9c1bb32527
jpetersen5 <jasonpetersen707@gmail.com>
2024-06-26 16:36:20 -0700

    ???

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,9 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dmbot/",
+  base: "/DMBot/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
   }
 })
