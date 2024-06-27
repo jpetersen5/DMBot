d0e54137ea3022619781c4b1b1039e4a603c3029
jpetersen5 <jasonpetersen707@gmail.com>
2024-06-26 20:07:32 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from "./components/Homepage";
+import Homepage from "./components/homepage";
 import AuthCallback from "./components/auth/AuthCallback";
 import "./App.css"
 
