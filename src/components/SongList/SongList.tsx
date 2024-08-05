e4f6e1d0ca872f09ee44d1fae539fb03ccbc92ce
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-08-05 12:06:57 -0700

    uggggh

--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -191,6 +191,7 @@ const SongTableCell: React.FC<SongTableCellProps> = ({ content }) => {
 
   const processedContent = typeof content === "string" 
     ? content.replace(/<color=(#[0-9A-Fa-f]{3,6})>(.*?)<\/color>/g, (match, color, text) => {
+        console.log(match, color, text);
         const fullColor = color.length === 4 
           ? `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${color[3]}` 
           : color;
