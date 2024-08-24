e028633d9f97bca08b42d7ef9810624c2758ab92
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-08-24 16:04:42 -0700

    wait

--- backend/app/api/songs.py
+++ backend/app/api/songs.py
@@ -167,6 +167,7 @@ def get_songs():
                 if matching_charters:
                     charter_array = "\{" + ",".join(f'"{charter}"' for charter in matching_charters) + "\}"
                     query = query.contains("charter_refs", charter_array)
+                    logger.info(f"Filtering by charter: {charter_array}")
                 
             elif filter in ["name", "artist", "album", "year", "genre"]:
                 query = query.ilike(filter, f"*{search}*")
