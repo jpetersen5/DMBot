6d2a56448117aacd8cc5d52a47d62602c431bc9c
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-08-24 16:19:57 -0700

    maybemaybemaybe

--- backend/app/api/songs.py
+++ backend/app/api/songs.py
@@ -165,7 +165,7 @@ def get_songs():
                 matching_charters = [charter["name"] for charter in charters_response.data]
                 
                 if matching_charters:
-                    charter_array = "\{" + ",".join(f'"{charter}"' for charter in matching_charters) + "\}"
+                    charter_array = "\\{" + ",".join(f'"{charter}"' for charter in matching_charters) + "\\}"
                     query = query.contains("charter_refs", charter_array)
                     logger.info(f"Filtering by charter: {charter_array}")
                 
