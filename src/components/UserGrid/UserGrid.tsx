e6feae2bf5763a4bf8e82e8a11a22634b9377bf9
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-09-02 17:02:15 -0700

    fix null pfp and stupid flickery tooltip bug

--- src/components/UserGrid/UserGrid.tsx
+++ src/components/UserGrid/UserGrid.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import { API_URL } from "../../App";
 import "./UserGrid.scss";
 import { getUserImage } from "../../utils/user";
@@ -26,20 +25,15 @@ const UserGrid: React.FC = () => {
       <h2>Authenticated Users</h2>
       <div className="user-list">
         {users.map(user => (
-          <OverlayTrigger
-            key={user.id}
-            placement="top"
-            overlay={<Tooltip id={`tooltip-${user.id}`}>{user.username}</Tooltip>}
-          >
-            <Link to={`/user/${user.id}`} className="user-grid-avatar">
-              <img 
-                src={getUserImage(user)} 
-                alt={user.username}
-                width="32"
-                height="32"
-              />
-            </Link>
-          </OverlayTrigger>
+          <Link to={`/user/${user.id}`} className="user-grid-avatar">
+            <img 
+              src={getUserImage(user)} 
+              alt={user.username.charAt(0).toUpperCase()}
+              width="32"
+              height="32"
+            />
+            <span className="tooltip">{user.username}</span>
+          </Link>
         ))}
       </div>
     </div>
--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,9 +1,27 @@
 export interface User {
   id: string;
   username: string;
-  avatar: string;
+  avatar: string | null;
 }
 
-export const getUserImage = (user: User) => {
-  return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
-}
+const getColorFromId = (id: string): string => {
+  const hash = id.split('').reduce((acc, char) => {
+    acc = ((acc << 5) - acc) + char.charCodeAt(0);
+    return acc & acc;
+  }, 0);
+  return `hsl(${hash % 360}, 70%, 60%)`;
+};
+
+export const getUserImage = (user: User): string => {
+  if (user.avatar) {
+    return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+  }
+  return `data:image/svg+xml,${encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+      <rect width="32" height="32" fill="${getColorFromId(user.id)}"/>
+      <text x="50%" y="50%" dy=".35em" fill="white" font-family="Arial" font-size="16" text-anchor="middle">
+        ${user.username.charAt(0).toUpperCase()}
+      </text>
+    </svg>
+  `)}`;
+};
