ab10eb52baa858b43412f9ed03645d67e5c74b96
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-09-06 14:52:48 -0700

    update version

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -36,7 +36,7 @@ const Sidebar: React.FC = () => {
 
   const navItems = user ? [...staticNavItems, profileNavItem] : staticNavItems;
 
-  const version = "v1.2.0";
+  const version = "v1.2.1";
   const commitDate = import.meta.env.VITE_COMMIT_DATE || "Unknown";
 
   return (
