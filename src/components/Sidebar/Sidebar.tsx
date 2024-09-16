12930885d2700889c905196c336e14b2ffacffb7
jason-petersen-at-hypothetic <jason.petersen@hypothetic.art>
2024-09-15 20:14:27 -0700

    v1.5.0

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -58,7 +58,7 @@ const Sidebar: React.FC = () => {
 
   const navItems = user ? [...staticNavItems, profileNavItem] : staticNavItems;
 
-  const version = "v1.4.20.69";
+  const version = "v1.5.0";
   const commitDate = import.meta.env.VITE_COMMIT_DATE || "Unknown";
 
   return (
